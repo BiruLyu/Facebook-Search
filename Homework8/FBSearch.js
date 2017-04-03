b110fbab5af987a633fa6351d08a8994e4117356
Biru Lyu <blyu@usc.edu>
2017-04-02 20:42:43 -0700

    back before trying to add animation

--- Homework8/FBSearch.js
+++ Homework8/FBSearch.js
@@ -35,56 +35,61 @@ myApp.controller('namesCtrl', function($scope, $rootScope, $http, $log) {
         //alert(response.data);
         
         $scope.users = response.data.users;
-        $scope.pages = response.data.pages.data;
-        $scope.events = response.data.events.data;
-        $scope.places = response.data.places.data;
-        $scope.groups = response.data.groups.data;
+        $scope.pages = response.data.pages;
+        $scope.events = response.data.events;
+        $scope.places = response.data.places;
+        $scope.groups = response.data.groups;
         $('#loadingmessage').hide();
-        
-        if($scope.users.paging.previous === undefined){
-            alert(1);
-            $('#goNext').show();
-            $('#goPrevious').hide();
-        } else if ( $scope.users.paging.next === undefined) {
-            alert(2);
-            $('#goNext').hide();
-            $('#goPrevious').show();
-        } else {
-            alert(3);
-            $('#goNext').show();
-            $('#goPrevious').show();
-        }
-        
+
+        $scope.myTable = $scope.users; 
     }
     );
     };
     
     
+//    $scope.tabs = [
+//        {title: 'Users',url: 'page-Users.html'}, 
+//        {title: 'Pages',url: 'page-Pages.html'}, 
+//        {title: 'Events',url: 'page-Events.html'},
+//        {title: 'Places',url: 'page-Places.html'}, 
+//        {title: 'Groups',url: 'page-Groups.html'}, 
+//        {title: 'Favorites',url: 'page-Favorites.html'}
+//    ];
+    
     $scope.tabs = [
-        {title: 'Users',url: 'page-Users.html'}, 
-        {title: 'Pages',url: 'page-Pages.html'}, 
-        {title: 'Events',url: 'page-Events.html'},
-        {title: 'Places',url: 'page-Places.html'}, 
-        {title: 'Groups',url: 'page-Groups.html'}, 
-        {title: 'Favorites',url: 'page-Favorites.html'}
+        {title: 'Users', content: $scope.users }, 
+        {title: 'Pages', content: $scope.pages }, 
+        {title: 'Events', content: $scope.events },
+        {title: 'Places', content: $scope.places }, 
+        {title: 'Groups', content: $scope.groups }, 
+        {title: 'Favorites', content: $scope.favorites }
     ];
     
+    
     $scope.currentTab = 'one.tpl.html';
-
+    
+    $scope.myTable = $scope.users; 
+    $scope.flag = 'Users';
     $scope.onClickTab = function (tab) {
-        $scope.currentTab = tab.url;
- 
+       // $scope.currentTab = tab.url;
+        if(tab.title === 'Users') {$scope.myTable = $scope.users; $scope.flag = 'Users';}
+        else if(tab.title === 'Pages') {$scope.myTable = $scope.pages;$scope.flag = 'Pages';}
+        else if(tab.title === 'Events') {$scope.myTable = $scope.events;$scope.flag = 'Events';}
+        else if(tab.title === 'Places') {$scope.myTable = $scope.places;$scope.flag = 'Places';}
+        else if(tab.title === 'Groups') {$scope.myTable = $scope.groups;$scope.flag = 'Groups';}
+        else if(tab.title === 'Favorites') {$scope.myTable = $scope.favorites;$scope.flag = 'Favorites';}
         
-        if($scope.users.paging.previous === undefined){
-            alert(1);
+        
+        if($scope.myTable.paging.previous === undefined){
+            //alert(1);
             $('#goNext').show();
             $('#goPrevious').hide();
-        } else if ( $scope.users.paging.next === undefined) {
-            alert(2);
+        } else if ( $scope.myTable.paging.next === undefined) {
+            //alert(2);
             $('#goNext').hide();
             $('#goPrevious').show();
         } else {
-            alert(3);
+            //alert(3);
             $('#goNext').show();
             $('#goPrevious').show();
         }
@@ -116,39 +121,68 @@ myApp.controller('namesCtrl', function($scope, $rootScope, $http, $log) {
     
    
     
-    $scope.PageChanging = function(tab,previousUrl, nextUrl){
+    $scope.PageChanging = function(Flag,Url){
         
-
-        
-        var config;
-        if(tab === 'Previous'){
-            config = {params: {url: previousUrl}};           
+//
+//        $.getJSON(Url, function(response) {
+//        
+//        $scope.myTable = response;
+//
+//        if(Flag === 'Users') {$scope.users = $scope.myTable;}
+//        else if(Flag === 'Pages') {$scope.pages = $scope.myTable;}
+//        else if(Flag === 'Events') {$scope.events = $scope.myTable;}
+//        else if(Flag === 'Places') {$scope.places = $scope.myTable;}
+//        else if(Flag === 'Groups') {$scope.groups = $scope.myTable;}
+//        else if(Flag === 'Favorites') {$scope.favorites = $scope.myTable;}
+//
+//        if($scope.myTable.paging.previous === undefined){
+//            //alert(1);
+//            $('#goNext').show();
+//            $('#goPrevious').hide();
+//        } else if ( $scope.myTable.paging.next === undefined || $scope.myTable.data.length < 25) {
+//            //alert(2);
+//            $('#goNext').hide();
+//            $('#goPrevious').show();
+//        } else {
+//            //alert(3);
+//            $('#goNext').show();
+//            $('#goPrevious').show();
+//        }           
+//});
+        var config= {params: {url: Url}};  
             
-        } else {
-            config = {params: {url: nextUrl}};
-              
-        }
         
-        $http.get("index2.php",config).then(function(response){
+        
+        $http.get(Url).then(function(response){
 
-                $scope.users = response.data;
-                if($scope.users.paging.previous === undefined){
-                    alert(1);
+                $scope.myTable = response.data;
+            
+                if(Flag === 'Users') {$scope.users = $scope.myTable;}
+                else if(Flag === 'Pages') {$scope.pages = $scope.myTable;}
+                else if(Flag === 'Events') {$scope.events = $scope.myTable;}
+                else if(Flag === 'Places') {$scope.places = $scope.myTable;}
+                else if(Flag === 'Groups') {$scope.groups = $scope.myTable;}
+                else if(Flag === 'Favorites') {$scope.favorites = $scope.myTable;}
+            
+                if($scope.myTable.paging.previous === undefined){
+                    //alert(1);
                     $('#goNext').show();
                     $('#goPrevious').hide();
-                } else if ( $scope.users.paging.next === undefined || $scope.users.data.length < 25) {
-                    alert(2);
+                } else if ( $scope.myTable.paging.next === undefined || $scope.myTable.data.length < 25) {
+                    //alert(2);
                     $('#goNext').hide();
                     $('#goPrevious').show();
                 } else {
-                    alert(3);
+                    //alert(3);
                     $('#goNext').show();
                     $('#goPrevious').show();
                 }
                 
-            
             }
         );
+        
+        
+
          
     };
 });
