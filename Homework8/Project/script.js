e2248056a84e9999cd6340a07301bad9077b457d
Biru Lyu <blyu@usc.edu>
2017-04-04 00:44:02 -0700

    Added Favorite Part

--- Homework8/Project/script.js
+++ Homework8/Project/script.js
@@ -4,6 +4,7 @@
 var app = angular.module('myApp', ['ngAnimate']);
 app.controller('animationsCtrl', function ($scope, $http, $log) {
     "use strict";
+    //localStorage.clear();
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
             $scope.latitude = position.coords.latitude;
@@ -41,32 +42,69 @@ app.controller('animationsCtrl', function ($scope, $http, $log) {
     ];
     $scope.currentTab = 'one.tpl.html';
     $scope.myTable = $scope.users;
-    $scope.flag = 'Users';
+    $scope.flag = 'users';
+    
+    ////////////////////Click Tab ////////////////////////////
     $scope.onClickTab = function (tab) {
+        $scope.ngSwitchSelected = 'item1';
         // $scope.currentTab = tab.url;
         if (tab.title === 'Users') {
+            //$('.type').hide();
+            $scope.activeFavorite = false;
             $scope.myTable = $scope.users;
-            $scope.flag = 'Users';
+            $scope.flag = 'users';
         }
         else if (tab.title === 'Pages') {
+            //$('.type').hide();
+            $scope.activeFavorite = false;
             $scope.myTable = $scope.pages;
-            $scope.flag = 'Pages';
+            $scope.flag = 'pages';
         }
         else if (tab.title === 'Events') {
+            //$('.type').hide();
+            $scope.activeFavorite = false;
             $scope.myTable = $scope.events;
-            $scope.flag = 'Events';
+            $scope.flag = 'events';
         }
         else if (tab.title === 'Places') {
+            //$('.type').hide();
+            $scope.activeFavorite = false;
             $scope.myTable = $scope.places;
-            $scope.flag = 'Places';
+            $scope.flag = 'places';
         }
         else if (tab.title === 'Groups') {
+            //$('.type').hide();
+            $scope.activeFavorite = false;
             $scope.myTable = $scope.groups;
-            $scope.flag = 'Groups';
+            $scope.flag = 'groups';
         }
         else if (tab.title === 'Favorites') {
-            $scope.myTable = $scope.favorites;
-            $scope.flag = 'Favorites';
+            //$('.type').show();
+            $scope.activeFavorite = true;
+            var favorList = [],temp = [], tempPair={};
+            $scope.favorites = [];
+            $.each(localStorage, function(key, value){
+                
+                
+                temp = JSON.parse(value);
+                //alert(temp[0]);
+                $scope.favorites.push(temp);
+                //tempPair = { picture : temp[0],name:temp[1],type:temp[2]};
+                //favorList.push(tempPair);
+                //var temp2 = JSON.parse(localStorage.getItem(id));
+                //alert(temp[0]);
+                //alert(tempPair.picture);
+              // key magic
+              // value magic
+
+            });
+            
+            
+            //$scope.myTable = $scope.favorites;
+            
+            $scope.flag = 'favorites';
+            
+            
         }
         if ($scope.myTable.paging.previous === undefined) {
             //alert(1);
@@ -75,6 +113,7 @@ app.controller('animationsCtrl', function ($scope, $http, $log) {
         }
         else if ($scope.myTable.paging.next === undefined) {
             //alert(2);
+            
             $('#goNext').hide();
             $('#goPrevious').show();
         }
@@ -124,14 +163,70 @@ app.controller('animationsCtrl', function ($scope, $http, $log) {
         };
         $http.get("index2.php", config).then(function (response) {
             $scope.detail = response.data;
+            $('#loadingAlbum').hide();
+            $('#loadingPosts').hide();
         });
-        $('#loadingAlbum').hide();
-        $('#loadingPosts').hide();
+       
         //$rootScope.$emit("CallParentMethod", {path:'/page4',pageAnimationClass:'slideLeft'});
     };
     ///////////////////Back/////////////////////
     $scope.Back = function () {
         $scope.ngSwitchSelected = 'item1';
+        
+    };
+    ///////////////////FavoriteStyle/////////////////
+    
+    
+    
+    $scope.activeStarStyle = function(id){
+        var temp = {};
+        if (localStorage.getItem(id) === null) {
+            temp = { "color" : 'black'};
+            
+        } else {
+            temp = {'color' : '#FED800'};
+        }
+        return temp;
+    }; 
+    
+    $scope.activeStarClass = function(id){
+        var temp = [];
+        if (localStorage.getItem(id) === null) {
+            
+            temp.push('glyphicon');
+            temp.push('glyphicon-star-empty');
+            
+        } else {
+            
+            temp.push('glyphicon');
+            temp.push('glyphicon-star');
+            
+        }
+        return temp;
+    };
+    
+
+    ///////////////////Delete Favorite/////////////////
+    
+    $scope.DelFavorite = function(id){
+        localStorage.removeItem(id);
+        $scope.onClickTab({title:'Favorites'});
+    };
+    
+    ///////////////////Favorite/////////////////
+    $scope.Favorite = function(id, url, name, flag){
+        
+        if(localStorage.getItem(id) === null){
+            var a, temp = [];
+            //a = '#'+id;
+            //$(a).removeClass( "glyphicon-star-empty" ).addClass( "glyphicon-star" ).css('color', '#FED800');
+
+            temp.push(url, name, flag, id);
+            localStorage.setItem(id,JSON.stringify(temp));
+        } else {
+            localStorage.removeItem(id);
+        }
+    
     };
     ///////////////////PageChanging/////////////////////
     $scope.PageChanging = function (Flag, Url) {
@@ -142,22 +237,22 @@ app.controller('animationsCtrl', function ($scope, $http, $log) {
         };
         $http.get(Url).then(function (response) {
             $scope.myTable = response.data;
-            if (Flag === 'Users') {
+            if (Flag === 'users') {
                 $scope.users = $scope.myTable;
             }
-            else if (Flag === 'Pages') {
+            else if (Flag === 'pages') {
                 $scope.pages = $scope.myTable;
             }
-            else if (Flag === 'Events') {
+            else if (Flag === 'events') {
                 $scope.events = $scope.myTable;
             }
-            else if (Flag === 'Places') {
+            else if (Flag === 'places') {
                 $scope.places = $scope.myTable;
             }
-            else if (Flag === 'Groups') {
+            else if (Flag === 'groups') {
                 $scope.groups = $scope.myTable;
             }
-            else if (Flag === 'Favorites') {
+            else if (Flag === 'favorites') {
                 $scope.favorites = $scope.myTable;
             }
             if ($scope.myTable.paging.previous === undefined) {
